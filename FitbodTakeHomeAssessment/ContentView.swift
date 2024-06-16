4ad0cd99e69ae15ffa0d9ef22d96b84497be9003
Brayden Harris <brayden4012@gmail.com>
2024-06-16 12:45:15 -0500

    Initial Commit

---
+++ FitbodTakeHomeAssessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitbodTakeHomeAssessment
+//
+//  Created by Brayden Harris on 6/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitbodTakeHomeAssessment/FitbodTakeHomeAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitbodTakeHomeAssessmentApp.swift
+//  FitbodTakeHomeAssessment
+//
+//  Created by Brayden Harris on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FitbodTakeHomeAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
