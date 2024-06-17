6c201d9c21954fc8ec7ba84508d9ae21ae815012
Brayden Harris <brayden4012@gmail.com>
2024-06-17 09:53:36 -0500

    setup project

--- FitbodTakeHomeAssessment/FitbodTakeHomeAssessmentApp.swift
+++ FitbodTakeHomeAssessment/FitbodTakeHomeAssessmentApp.swift
@@ -5,13 +5,25 @@
 //  Created by Brayden Harris on 6/16/24.
 //
 
+import ExerciseDomain
+import ExerciseUI
 import SwiftUI
 
 @main
 struct FitbodTakeHomeAssessmentApp: App {
+    let networkService = NetworkService()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ExerciseListScreen(
+                    viewModel: ExerciseListViewModel(
+                        exerciseService: ExerciseService(
+                            networkService: networkService
+                        )
+                    )
+                )
+            }
         }
     }
 }
